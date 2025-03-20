be54466b8aac7837f0a577694375a870876ff65e
mariyancholakov <m.cholakov09@gmail.com>
2025-03-20 19:31:05 +0200

    Changed the routes a bit and added functionality in the frontend (doesnt work yet)

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,27 +1,42 @@
 import express from "express";
 import Recipe from "../models/recipeModel.js";
+import User from "../models/userModel.js";
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/:userId/saved", async (req, res) => {
   try {
-    const { name, description, ingredients, instructions } = req.body;
-    const newRecipe = new Recipe({
-      name,
-      description,
-      ingredients,
-      instructions,
-    });
-    await newRecipe.save();
-    res.status(201).json(newRecipe);
+    const { userId } = req.params;
+    const { recipeId } = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.savedRecipes.includes(recipeId)) {
+      return res.status(400).json({ message: "This recipe is already saved!" });
+    }
+
+    user.savedRecipes.push(recipeId);
+    await user.save();
+
+    res.status(201).json({ message: "Recipe saved successfully!" });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Server Error" });
   }
 });
 
-router.get("/:userId", async (req, res) => {
+router.get("/:userId/saved", async (req, res) => {
   try {
-    const recipes = await Recipe.find({ userId: req.params.userId });
-    res.status(200).json(recipes);
+    const { userId } = req.params;
+
+    const user = await User.findById(userId).populate("savedRecipes");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user.savedRecipes);
   } catch (error) {
     res.status(500).json({ error: "Server Error" });
   }
