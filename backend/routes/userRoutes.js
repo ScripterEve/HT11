7b769c453767f9f26791fbc10c79acbd990c93c5
mariyancholakov <m.cholakov09@gmail.com>
2025-03-21 01:29:46 +0200

    Created user route for updating user details

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,5 +14,26 @@ router.get("/:id", authMiddleware, async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+router.put("/:id", authMiddleware, async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { username, email, password, diseases, allergies } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(userId, {
+      username,
+      email,
+      password,
+      diseases,
+      allergies,
+    });
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error("Error updating user details:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 export default router;
