ad0194116e0b57d6c282d9ba366c23b6c7560397
mariyancholakov <m.cholakov09@gmail.com>
2025-03-20 12:28:03 +0200

    Created login and register pages and added routing in the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
-      <div>Hello</div>
+      Hello
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+      </Routes>
     </>
   );
 }
