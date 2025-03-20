a8c4ffef69753b8c127032174cb0401532c25c6e
mariyancholakov <m.cholakov09@gmail.com>
2025-03-20 13:15:30 +0200

    Created an authContext and added functionality to the login and register forms

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,17 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthContext from "../context/authContext";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      await login(email, password);
+      setLoading(false);
+      const token = localStorage.getItem("token");
+      if (token) {
+        alert("Login successful!");
+        navigate("/");
+      } else {
+        alert("Login failed!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="w-80 h-[60%] px-4 py-5 flex flex-col gap-4">
       <h2 className="text-3xl font-bold text-center mb-5">Login</h2>
-      <form className="flex flex-col gap-6">
+      <form onSubmit={handleLogin} className="flex flex-col gap-6">
         <div>
           <label className="ml-2 font-semibold">Email</label>
           <input
             className="py-1.5 pl-3 w-full mt-1 bg-transparent rounded-full border-2 focus:outline-none"
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div>
@@ -20,6 +48,8 @@ function LoginForm() {
             className="py-1.5 pl-3 w-full mt-1 bg-transparent rounded-full border-2 focus:outline-none"
             type="password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
@@ -27,7 +57,7 @@ function LoginForm() {
           className="py-2 mt-4 transition duration-300 ease-in-out cursor-pointer text-center border-2 text-lg font-semibold w-full rounded-full "
           type="submit"
         >
-          Login
+          {loading ? "Submitting" : "Login"}
         </button>
       </form>
       <div className="mt-4">
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,16 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 function RegisterForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:3000/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, email, password }),
+      });
+      if (!res.ok) {
+        console.log("Couldn't register user!");
+        setLoading(false);
+      }
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setLoading(false);
+      alert("Registration successful!");
+      navigate("/api/auth/login");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="w-80 h-[60%] px-4 py-5 flex flex-col gap-4">
       <h2 className="text-3xl font-bold text-center mb-5">Sign up</h2>
-      <form className="flex flex-col gap-6">
+      <form onSubmit={handleRegister} className="flex flex-col gap-6">
         <div>
           <label className="ml-2 font-semibold">Username</label>
           <input
             className="py-1.5 pl-3 w-full mt-1 rounded-full bg-transparent border-2 focus:outline-none"
             type="text"
             name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div>
@@ -19,6 +51,8 @@ function RegisterForm() {
             className="py-1.5 pl-3 w-full bg-transparent mt-1 rounded-full border-2 focus:outline-none"
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div>
@@ -27,6 +61,8 @@ function RegisterForm() {
             className="py-1.5 pl-3 w-full mt-1 bg-transparent rounded-full border-2 focus:outline-none"
             type="password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
@@ -34,7 +70,7 @@ function RegisterForm() {
           className="py-2 mt-4 border-2 transition duration-300 ease-in-out cursor-pointer text-center  text-lg font-semibold w-full rounded-full "
           type="submit"
         >
-          Sign Up
+          {loading ? "Submitting" : "Sign Up"}
         </button>
       </form>
       <p className="text-center font-light">
