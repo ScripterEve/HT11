d6dd1144f597e36047da61cc6771541bb4ff9154
Eva <e.ivaylova@gmail.com>
2025-03-21 21:26:12 +0200

    why was there an emoji??

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,5 @@ app.use("/api/recipes/", recipeRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
