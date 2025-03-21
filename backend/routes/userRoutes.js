91a8e37dfce433e315dbbf6a8030cf28080eff64
AleksandarPetrovv <aleksandar.z.petrov.2023@elsys-bg.org>
2025-03-21 15:25:13 +0200

    hi

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,18 @@ import authMiddleware from "../middleware/authMiddleware.js";
 import User from "../models/userModel.js";
 const router = express.Router();
 
+router.get("/:id", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 router.get("/:userId/saved-recipes", authMiddleware, async (req, res) => {
   try {
     const userId = req.params.userId;
