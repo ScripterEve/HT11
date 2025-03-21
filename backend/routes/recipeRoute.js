a961cc6b5589f1243ef3b8da458ee08e4e62e186
Eva <e.ivaylova@gmail.com>
2025-03-21 02:32:31 +0200

    Added recipeRoute through AI and fixed the urls for the AI requests

---
+++ backend/routes/recipeRoute.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import axios from 'axios'
+import dotenv from 'dotenv'
+import User from '../models/userModel.js'
+import { promptRecipe, promptRecipeWithoutDisease, promptRecipeWithoutAllergies, promptRecipeWithoutDiseaseAndAllergies } from "../common/constants.js";
+import  format  from "../common/utils.js";
+
+dotenv.config()
+
+const productRouter = express.Router()
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+
+
+productRouter.post('/ask', async (req, res) => {
+    const { food } = req.body;
+
+    try {
+        let prompt;
+        if (User.disease && User.allergies && User.disease?.length > 0 && User.allergies?.length > 0) {
+            prompt = format(promptRecipe, User.disease, User.allergies, food);
+        } else if (!User.disease && User.disease?.length <= 0) {
+            prompt = format(promptRecipeWithoutDisease.disease, User.allergies, food);
+        }
+        else if (!User.allergies && User.allergies?.length <= 0) {
+            prompt = format(promptRecipeWithoutAllergies, User.disease, food);
+        }
+        else {
+            prompt = format(promptRecipeWithoutDiseaseAndAllergies, food);
+        }
+
+        const response = await axios.post(
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+            {
+                contents: [{ parts: [{ text: prompt }] }]
+            },
+            { headers: { "Content-Type": "application/json" } }
+        );
+
+        const botResponse = response.data.candidates[0].content.parts[0].text;
+        res.json({ answer: botResponse });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+export default productRouter;
